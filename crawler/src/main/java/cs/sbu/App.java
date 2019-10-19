d214133d2edd87bd2c68b18df1f4f918f64efc0f
m.rastegar <m.rastegar@dornevis.com>
2019-10-19 23:32:32 +0330

    create configuration and base of project

--- crawler/src/main/java/cs/sbu/App.java
+++ crawler/src/main/java/cs/sbu/App.java
@@ -1,13 +1,19 @@
 package cs.sbu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import cs.sbu.config.AppConfig;
+import cs.sbu.crawl.Crawler;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        AppConfig appConfig = new AppConfig();
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue<String>(Integer.parseInt(appConfig.getProperty("maxurlinqueue")));
+        arrayBlockingQueue.add("https://en.wikipedia.org/wiki/Main_Page");
+        for (int i = 0; i < Integer.parseInt(appConfig.getProperty("threadnumber")); i++) {
+            Thread thread = new Thread(new Crawler(arrayBlockingQueue));
+        }
     }
 }
